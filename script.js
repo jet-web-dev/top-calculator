330a5b77435f91a40e2a45e33f496c3a2ab28494
jet-web-dev <webdev.jet@gmail.com>
2023-02-28 14:39:58 -0800

    Create initial structure, style, and functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+function add (a,b) {
+    return a + b;
+}
+
+function subtract (a,b) {
+    return a - b;
+}
+
+function multiply (a,b) {
+    return a * b;
+}
+
+function divide (a,b) {
+    return a / b;
+}
+
+let a = 0;
+let b = 0;
+let result = 0;
+let opp = "";
+
+function operate (opp,a,b) {
+    switch(opp) {
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a,b);
+            break;
+        case "*":
+            result = multiply(a,b);
+            break;
+        case "/":
+            result = divide(a,b);
+            break;
+    }
+}
