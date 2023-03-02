6496c1d750ce1e63ca718d458651303a257c7286
jet-web-dev <webdev.jet@gmail.com>
2023-03-02 13:20:19 -0800

    Add functionality to all buttons, finalize submission

--- script.js
+++ script.js
@@ -1,3 +1,101 @@
+let num1 = null;
+let num2 = null;
+let result = null;
+let operator = "";
+let displayValue = "0";
+let lastClick = "";
+const buttons = document.querySelectorAll("button");
+const display = document.getElementById("calc-display");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        if (buttons[i].classList.contains('btn-num')) {
+            numClick(buttons[i].innerText);
+        } else if (buttons[i].classList.contains('btn-dec')) {
+            decClick(buttons[i].innerText);
+        } else if (buttons[i].classList.contains('btn-clr')) {
+            clrClick();
+        } else if (buttons[i].classList.contains('btn-bck')) {
+            bckClick();
+        } else if (buttons[i].classList.contains('btn-eql')) {
+            eqlClick(displayValue);
+        } else if (buttons[i].classList.contains('btn-opr')) {
+            oprClick(displayValue, buttons[i].innerText)
+        } else {
+            alert("Error in button event listener assignment!")
+        }
+    })
+};
+
+function displayUpdate () {
+    if (displayValue.length > 10) {
+        display.innerText = displayValue.substring(0, 10);
+    } else {
+        display.innerText = displayValue;
+    }
+}
+
+function numClick (a) {
+    if(displayValue === "0" || displayValue == num1) {
+        displayValue = a;
+        displayUpdate(displayValue);
+        lastClick = "num";
+    } else {
+        displayValue += a;
+        displayUpdate(displayValue);
+        lastClick = "num";
+    }
+}
+
+function decClick (a) {
+    if (displayValue.includes(".") === false) {
+        displayValue += a;
+        displayUpdate(displayValue);
+        lastClick = "dec";
+    }
+}
+
+function clrClick () {
+    num1 = null;
+    num2 = null;
+    result = null;
+    operator = "";
+    displayValue = "0";
+    lastClick = "";
+    displayUpdate(displayValue);    
+}
+
+function bckClick() {
+    if (+displayValue > 0) {
+        displayValue = displayValue.slice(0, -1);
+        displayUpdate(displayValue);
+    }
+}
+
+function eqlClick (a) {
+    if (operator === "/" && +a === 0) {
+        alert("You can't divide by 0, you CAD!");
+    } else if ( num1 !== null && operator !== "") {
+    num2 = +a;
+    result = operate(num1, num2, operator);
+    num1 = result;
+    displayValue = result;
+    displayUpdate(displayValue);
+    }    
+}
+
+function oprClick (a, b) {
+    if (operator !== "") {
+        eqlClick(displayValue);
+        operator = b;
+    } else {
+        num1 = +a;
+        operator = b;
+        lastClick = "num"
+    }
+
+}
+
 function add (a,b) {
     return a + b;
 }
@@ -7,38 +105,26 @@ function subtract (a,b) {
 }
 
 function multiply (a,b) {
-    return a * b;
+    return Math.round(a * b * 100) / 100;
 }
 
 function divide (a,b) {
-    return a / b;
+    return Math.round(a / b * 100) / 100;
 }
 
-function remainder (a,b) {
-    return a % b;
-}
-
-let a = 0;
-let b = 0;
-let result = 0;
-let opp = "";
-
-function operate (opp,a,b) {
+function operate (a,b,opp) {
     switch(opp) {
         case "+":
-            result = add(a,b);
+            return add(a,b);
             break;
         case "-":
-            result = subtract(a,b);
+            return subtract(a,b);
             break;
         case "*":
-            result = multiply(a,b);
+            return multiply(a,b);
             break;
         case "/":
-            result = divide(a,b);
-            break;
-        case "%":
-            result = remainder(a,b);
+            return divide(a,b);
             break;
     }
 }
